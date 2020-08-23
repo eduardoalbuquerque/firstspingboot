90ecd66d39ceb38e602082e02f0c5967d45232eb
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-08-23 17:06:41 -0300

    first commit

---
+++ src/test/java/br/com/webdevbrasil/course/Course2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webdevbrasil.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
